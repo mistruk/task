d8f8bbae1cd47974e85d1c1e4fb556ed750d2c00
mistruk <114093441+mistruk@users.noreply.github.com>
2022-10-05 18:32:11 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	//задание один
+	int hp;
+
+	bool a;
+	cout << "введите число " << endl;
+	cin >> hp;
+	if ( hp > 0)
+	{
+		a = hp > 0;
+		cout << a << endl;
+	}
+	else
+	{
+		a = hp < 0;
+		cout << a << endl;
+	}
+	// задание 2
+	//int a;
+	//cout << "введите число " << endl;
+	//cin >> a;
+	//if (a > 20)
+	//{
+	//	cout << "on больше 20 градусов" << endl;
+	//}
+	//else
+	//{
+	//	cout << "off температура меньше 20 градусов" << endl;
+	//}
+	// задание 3
+	//int a;
+	//int b;
+	//int t;
+
+	//cout << "введите определеннную температуру" << endl;
+	//cin >> t;
+	//cout << "темепература в комнате сейчас " << endl;
+//	cin >> a;
+	//cout << "влажность сейчас" << endl;
+	//cin >> b;
+//	if (b > a && t < 80)
+//	{
+//		cout << "on" << endl;
+	//}
+//	else
+//	{
+//		cout << "of" << endl;
+	//int a;
+	//cout << "введите число " << endl;
+	//cin >> a;
+	//}
+
+
+	
+
+
+
+
+
+}
+
+
+		
